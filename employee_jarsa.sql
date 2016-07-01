6b4543e1631340f7e54e79eb337a73573e54c476
RicardoEscobedo <griki00@hotmail.com>
2016-07-01 17:25:09 -0500

    Update employee_jarsa.sql
    Ricardo Escobedo

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -32,5 +32,37 @@ INSERT INTO employee VALUES('E2','Diego','Garcia','D3');
 INSERT INTO employee VALUES('E3','Jose','Martinez','D4');
 INSERT INTO employee VALUES('E4','Ernesto','Gonzalez','D6');
 
+CREATE TABLE employee_hobby
+(
+  id character varying(10) NOT NULL,
+  name character varying(25) NOT NULL,
+  description character varying(50),
+  CONSTRAINT "PK_hobbyID" PRIMARY KEY (id)
+)
+
+INSERT INTO employee_hobby VALUES('H1','Reading','Likes to read books');
+INSERT INTO employee_hobby VALUES('H2','Sports','Practices a sport');
+INSERT INTO employee_hobby VALUES('H3','Board Games','Likes to play board games');
+
+CREATE TABLE rs_employee_hobby
+(
+  "employeeID" character varying(10) NOT NULL,
+  "hobbyID" character varying(10) NOT NULL,
+  CONSTRAINT "PK_rsEH" PRIMARY KEY ("employeeID", "hobbyID"),
+  CONSTRAINT "FK_employeeID" FOREIGN KEY ("employeeID")
+      REFERENCES employee (id),
+  CONSTRAINT "FK_hobbyID" FOREIGN KEY ("hobbyID")
+      REFERENCES employee_hobby (id)
+)
+
+INSERT INTO rs_employee_hobby VALUES('E1','H1');
+INSERT INTO rs_employee_hobby VALUES('E1','H2');
+INSERT INTO rs_employee_hobby VALUES('E2','H1');
+INSERT INTO rs_employee_hobby VALUES('E2','H3');
+INSERT INTO rs_employee_hobby VALUES('E3','H2');
+INSERT INTO rs_employee_hobby VALUES('E3','H3');
+INSERT INTO rs_employee_hobby VALUES('E4','H3');
+INSERT INTO rs_employee_hobby VALUES('E4','H1');
+
 
 -- ...
